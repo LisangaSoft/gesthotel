c2835b9028e5034df5ce3e3b73e86b3e9a140f06
junomap <mavericspro@gmail.com>
2017-10-30 16:48:19 +0100

    modification de la nouvelle structure :template et service(pas fonctionnel)

--- client/app.js
+++ client/app.js
@@ -1,17 +1,26 @@
+
 angular.module('hotel')
-    .config(['$routeProvider',function ($routeProvider) {
+    .config(['$routeProvider',function ($routeProvider) {    
         $routeProvider
             .when("/", {
-                templateUrl: "partials/home/home.html"
-            })
-            .when("/user", {
-                templateUrl: "partials/user/user.html",
-                controller: 'UserController as userCtrl'
-            })
-            .when("/auth", {
-                templateUrl: "partials/auth/auth.html"
-            })
+                
+                templateUrl: "partials/login/login.html"
+                //controller: 'LoginController as loginCtrl'
+                })
+            
+            .when("/home", {
+               templateUrl: "partials/home/home.html"
+                })
+            .when("/reservation", {
+               templateUrl: "partials/reservation/reservation.html",
+               controller: 'ReservationController as reservationCtrl'
+                })
+            .when("/client", {
+               templateUrl: "partials/client/client.html",
+               controller: 'ClientController as clientCtrl'
+                })
             .otherwise({
                 redirectTo: '/'
-            });
-    }]);
+                });
+    }])
+  
